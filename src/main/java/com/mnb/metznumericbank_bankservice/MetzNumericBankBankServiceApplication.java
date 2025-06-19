20aa598c106aec73222e7b770f74c9c4ad68cfe5
Layp <frau.nicolas7@gmail.com>
2025-06-19 15:01:28 +0200

    First commit

---
+++ src/main/java/com/mnb/metznumericbank_bankservice/MetzNumericBankBankServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mnb.metznumericbank_bankservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetzNumericBankBankServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetzNumericBankBankServiceApplication.class, args);
+    }
+
+}
